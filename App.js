25f92a2970272188b5efad1efa8d3168e75b7981
Guilherme <joseserafimtavares89@gmail.com>
2022-03-16 16:47:41 +0000

    Olá1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ola!</Text>
+      <Text>Olá!</Text>
       <StatusBar style="auto" />
     </View>
   );
