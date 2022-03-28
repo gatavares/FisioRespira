f1e68be82010390c75b7f85208d820e97e004899
Guilherme <joseserafimtavares89@gmail.com>
2022-03-28 09:12:20 +0100

    Update App.js

--- App.js
+++ App.js
@@ -25,7 +25,6 @@ export default function App() {
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Home" component={HomeScreen} />
           </Stack.Navigator>
-          
         </NavigationContainer>
         
       </ApplicationProvider>
