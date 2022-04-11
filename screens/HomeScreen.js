49e986375c983a9108e5c0204b4de9d13647ea72
Guilherme <joseserafimtavares89@gmail.com>
2022-04-11 15:46:51 +0100

    exMenu Finished

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, View  } from 'react-native'
+import { StyleSheet, ScrollView, View } from 'react-native'
 import React from 'react'
 import { Layout, Text } from '@ui-kitten/components';
 
@@ -20,43 +20,47 @@ const HomeScreenRender = () => {
         <View style={{ margin: '7%', }}>
           <Text category='s1' style={{ fontSize: 22, }}>O QUE É FISIOTERAPIA RESPIRATÓRIA?</Text>
           <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            {'\n'}{'\t'}O Programa de Reabilitação Respiratória é um tratamento médico multidimensional implementado por uma equipa multiprofissional
+            e multidisciplinar para pacientes com doenças respiratórias crônicas sintomáticas. {'\n'}{'\n'}Inclui estratégias abrangentes e
+            individualizadas para cada paciente com o objetivo de reduzir a dispneia (falta de ar) e melhorar a respiração,
+            aumentar a tolerância ao esforço, o engajamento social e a qualidade de vida, além de reduzir os custos de saúde.
+            E reduzir uma série de fatores que contribuem para a evolução natural da doença.
           </Text>
-          <Text category='s1' style={styles.title}>UTILIDADES</Text>
+
+
+          <Text category='s1' style={styles.title}>UTILIDADES E VANTAGENS</Text>
+          <Text category='s1' style={{ fontSize: 18, marginTop: 8, textDecorationLine: 'underline' }}>NAS DOENÇAS CRONICAS:</Text>
           <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            {'\n'}{'\t'}A fisioterapia atua na fase aguda de uma doença, seja na unidade de terapia intensiva, pacientes hospitalizados ou em casa para:{'\n'}
+            <Text style={{ fontWeight: 'bold', }}>
+              {'\n'}- Aliviar os sintomas, como dificuldade para respirar{'\n'}
+              {'\n'}- Melhorar o efeito da tosse{'\n'}
+              {'\n'}- Aumentar os níveis de ventilação e oxigénio{'\n'}
+              {'\n'}- Aumento da mobilidade e expansão pulmonar{'\n'}
+              {'\n'}- Prevenção de deformidades posturais associadas a doenças respiratórias{'\n'}
+              {'\n'}- Desobstruir as vias aéreas e evitar o acúmulo de secreções{'\n'}
+            </Text>
           </Text>
-          <Text category='s1' style={styles.title}>VANTAGENS</Text>
+          <Text category='s1' style={{ fontSize: 18, marginTop: 8, textDecorationLine: 'underline' }}>NA FASE ESTÁVEL:</Text>
           <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            {'\n'}{'\t'}Atua com o objetivo de:{'\n'}
+            <Text style={{ fontWeight: 'bold', }}>
+              {'\n'}- Prevenir deterioração e hospitalização futura{'\n'}
+              {'\n'}- Maior tolerância ao esforço{'\n'}
+              {'\n'}- Estratégias de ensino para controlo da dispneia{'\n'}
+              {'\n'}- Ensinar sobre fatores de risco{'\n'}
+              {'\n'}- Ganhar confiança e autonomia no dia a dia{'\n'}
+              {'\n'}- Prevenir o isolamento e a depressão devido às limitações da doença{'\n'}
+              {'\n'}- Aumentar a esperança e a qualidade de vida de cada usuário{'\n'}
+            </Text>
           </Text>
+
+
           <Text category='s1' style={styles.title}>PARA QUEM É RECOMENDADO E DIRECIONADO?</Text>
           <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            {'\n'}{'\t'}A fisioterapia respiratória pode ser realizada em adultos e na idade pediátrica, como bebes ou crianças.
+            {'\n'}{'\n'}A fisioterapia respiratória pode ser utilizada em pacientes com doença pulmonar obstrutiva crónica, bronquiectasias, 
+            fibrose cística, bronquelite, no pré e pós-operatório de cirurgia cardiotorácica e abdominal alta, entre outras condições.
           </Text>
         </View>
       </ScrollView>
--- screens/Pages/ExerciciosPage.js
+++ screens/Pages/ExerciciosPage.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, ScrollView, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import React from 'react'
 import { Layout, Text, Icon } from '@ui-kitten/components';
 
@@ -25,52 +25,172 @@ const exerciciosRenderContent0 = () => {
 }
 
 const exerciciosRenderContent1 = () => {
+  function MenuTypeEx() {
+    const [typeExVisible, settypeExVisible] = React.useState(true);
+    const [typeEx1, settypeEx1] = React.useState(true);
+    const [typeEx2, settypeEx2] = React.useState(false);
+
+    function TextSelecMenu() {
+      return (
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Text category='s1' style={{ marginBottom: 15, marginTop: 15, }}>SELECIONE O TIPO DE EXERCICIOS QUE DESEJA.</Text>
+        </View>
+      )
+    }
+
+    if (typeEx1 == true || typeEx2 == false) {
+      if (typeExVisible == true) {
+        return (
+          <>
+            <View style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 10, }}>
+              <TextSelecMenu />
+              <View style={{ flexDirection: 'row', }}>
+                <TouchableOpacity
+                  style={{ marginRight: 20 }}
+                  onPress={() => {
+                    settypeEx1(true)
+                    settypeEx2(false)
+                    settypeExVisible(false)
+                  }}>
+                  <Text
+                    style={{ textAlign: 'center', fontSize: 18, color: '#0099a7', fontWeight: 'bold', textDecorationLine: 'underline', textDecorationColor: '#0099a7' }}>
+                    Fisioterapira{'\n'}Respiratória
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{ marginLeft: 20 }}
+                  onPress={() => {
+                    settypeEx2(true)
+                    settypeEx1(false)
+                    settypeExVisible(false)
+                  }}>
+                  <Text
+                    style={{ textAlign: 'center', fontSize: 19, }}>
+                    Exercicios{'\n'}Fisicos
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <ScrollView>
+              <View style={{ margin: '7%', }}>
+                <Text category='s1' style={{ fontSize: 22, }}>EXERCICIOS 1</Text>
+                <Text category='p2' style={styles.text}>
+                  {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+              </View>
+            </ScrollView>
+          </>
+        )
+      }
+      else {
+        return (
+          <>
+            <View style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 10, marginTop: 20, }}>
+              <View style={{ flexDirection: 'row', }}>
+                <TouchableOpacity
+                  style={{ marginRight: 20 }}
+                  onPress={() => {
+                    settypeEx2(true)
+                    settypeEx1(false)
+                    settypeExVisible(false)
+                  }}>
+                  <Text
+                    style={{ textAlign: 'center', fontSize: 18, color: '#0099a7', fontWeight: 'bold', textDecorationLine: 'underline', textDecorationColor: '#0099a7' }}>
+                    Fisioterapira{'\n'}Respiratória
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{ marginLeft: 20 }}
+                  onPress={() => {
+                    settypeEx2(true)
+                    settypeEx1(false)
+                    settypeExVisible(false)
+                  }}>
+                  <Text
+                    style={{ textAlign: 'center', fontSize: 19, }}>
+                    Exercicios{'\n'}Fisicos
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <ScrollView>
+              <View style={{ margin: '7%', }}>
+                <Text category='s1' style={{ fontSize: 22, }}>EXERCICIOS 2</Text>
+                <Text category='p2' style={styles.text}>
+                  {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+              </View>
+            </ScrollView>
+          </>
+        )
+      }
+    }
+    if (typeEx1 == false || typeEx2 == true) {
+      return (
+        <>
+          <View style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 10, marginTop: 20, }}>
+            <View style={{ flexDirection: 'row', }}>
+              <TouchableOpacity
+                style={{ marginRight: 20 }}
+                onPress={() => {
+                  settypeEx1(true)
+                  settypeEx2(false)
+                  settypeExVisible(false)
+                }}>
+                <Text
+                  style={{ textAlign: 'center', fontSize: 19, }}>
+                  Fisioterapira{'\n'}Respiratória
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ marginLeft: 20 }}
+                onPress={() => {
+                  settypeEx1(true)
+                  settypeEx2(false)
+                  settypeExVisible(false)
+                }}>
+                <Text
+                  style={{ textAlign: 'center', fontSize: 18, color: '#0099a7', fontWeight: 'bold', textDecorationLine: 'underline', textDecorationColor: '#0099a7' }}>
+                  Exercicios{'\n'}Fisicos
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <ScrollView>
+            <View style={{ margin: '7%', }}>
+              <Text category='s1' style={{ fontSize: 22, }}>EXERCICIOS 3</Text>
+              <Text category='p2' style={styles.text}>
+                {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+              </Text>
+            </View>
+          </ScrollView>
+        </>
+      )
+    }
+  }
+
+
   return (
     <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-      <ScrollView>
-        <View style={{ margin: '7%', }}>
-          <Text category='s1' style={{ fontSize: 22, }}>EXERCICIOS</Text>
-          <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text category='s1' style={styles.title}>UTILIDADES</Text>
-          <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text category='s1' style={styles.title}>VANTAGENS</Text>
-          <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text category='s1' style={styles.title}>PARA QUEM É RECOMENDADO E DIRECIONADO?</Text>
-          <Text category='p2' style={styles.text}>
-            {'\n'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </View>
-      </ScrollView>
+      <MenuTypeEx />
     </Layout>
   )
 }
